0faa6ba9e9348d63995be5fc22371a944db46cd7
bigmosi <kinyeramo@gmail.com>
2023-01-25 10:50:44 +0300

    refactor speaker component

---
+++ src/components/Speakers.js
@@ -0,0 +1,21 @@
+import { data } from "../../SpeakerData";
+import Speaker from "./Speaker";
+
+ function Speakers() {
+    return (
+        <div className="container speaker-list">
+          <div className="row">
+            {
+                data.map(function(speaker) {
+                   
+                    return (
+                         <Speaker key={speaker.id} speaker={speaker} />
+                    );
+                })
+            }
+          </div>
+        </div>
+    );
+ }
+
+ export default Speakers;
