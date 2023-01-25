d1fbc8ea8dc427125536bdd54e0f394e703e51e2
bigmosi <kinyeramo@gmail.com>
2023-01-25 10:32:11 +0300

    designing react component

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { data } from "../SpeakerData";
+import Speaker from "../src/components/Speaker";
+
+const IndexPage = () => {
+
+    return (
+        <div className="container speaker-list">
+          <div className="row">
+            {
+                data.map(function(speaker) {
+                   
+                    return (
+                         <Speaker key={speaker.id} speaker={speaker} />
+                    );
+                })
+            }
+          </div>
+        </div>
+    );
+}
+
+export default IndexPage;
