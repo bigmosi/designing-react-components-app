b603799c373654468876ca98507a354acecf4e4a
bigmosi <kinyeramo@gmail.com>
2023-01-26 12:41:51 +0300

    passing data down via props

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e2ab63f0b78e20a3"; }
+/******/ 		__webpack_require__.h = function() { return "de523506174ec690"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -123,10 +123,15 @@ function SpeakerDemographics() {
 }
 
 function Speaker({ speaker, updateRecord, insertRecord, deleteRecord}) {
-  const { id, first, last, sessions } = speaker;
   const { showSessions } = useContext(SpeakerFilterContext);
+
   return (
-     <SpeakerProvider speaker={speaker} updateRecord={updateRecord}>
+     <SpeakerProvider 
+     speaker={speaker} 
+     updateRecord={updateRecord}
+     insertRecord={insertRecord}
+     deleteRecord={deleteRecord}
+     >
       <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
       <div className="card card-height p-4 mt-4">
         <SpeakerImage  />
