92f3c68ef401bf4b0a3edfece68cb46d12440d3d
bigmosi <kinyeramo@gmail.com>
2023-01-27 11:47:27 +0300

    wrap it's children

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,13 @@
 import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
+
+function AuthProvider({children, initialLoggedInUser}) {
+    const [loggedInUser, setLoggedInUser] = useState(initialLoggedInUser);
+
+    return (
+        <AuthContext.Provider value={{loggedInUser, setLoggedInUser}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
